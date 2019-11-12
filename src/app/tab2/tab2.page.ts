b2d8811077f57cb8f045fe506dde34b166b8f2a1
David Garcia <david.garcialopez@telefonica.com>
2019-11-12 10:52:50 +0100

    feat(button):Initial commit to test button issue

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
-
+  public slideOpts = {
+    initialSlide: 1,
+    speed: 500,
+    // centeredSlides: true,
+    // slidesPerView: 1,
+    // spaceBetween: 10,
+    // autoHeight: true,
+    // preventInteractionOnTransition: true,
+    // longSwipes: false,
+    pagination: {
+      type: "progressbar",
+      el: ".bar"
+    }
+  };
   constructor() {}
-
 }
